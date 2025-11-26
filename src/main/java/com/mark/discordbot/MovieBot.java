96751f1aa8803f67aefe88db740b08940c4674ce
MobDude <markobow@gmail.com>
2025-11-26 16:58:20 -0500

    Added structure for implemented slash commands

--- src/main/java/com/mark/discordbot/MovieBot.java
+++ src/main/java/com/mark/discordbot/MovieBot.java
@@ -2,7 +2,11 @@ package com.mark.discordbot;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
 import javax.security.auth.login.LoginException;
 
 
@@ -29,13 +33,58 @@ public class MovieBot extends ListenerAdapter
         JDABuilder.createDefault(token)
                 .setActivity(Activity.watching("Movie Nights"))
                 .addEventListeners(new MovieBot())
-                .build();
+                .build()
+                .updateCommands()
+                .addCommands(
+                        //add movie slash command
+                        Commands.slash("addmovie", "Adds a movie to the list")
+                                .addOption(OptionType.STRING, "name", "Movie title", true)
+                                .addOption(OptionType.INTEGER, "year", "Release year", false),
+
+                        //remove movie slash command
+                        Commands.slash("removemovie", "Removes a movie from the list")
+                                .addOption(OptionType.STRING, "query", "Part of the movie name", true),
+
+                        //show list slash command
+                        Commands.slash("movielist", "Shows the movie list")
+                )
+                .queue();
+
 
         System.out.println("MovieBot is now running!");
     }
 
     public static MovieStorage getStorage(){
         return  storage;
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
+        switch (event.getName()){
+            case "addmovie":
+                handleAddMovie(event);
+                break;
+
+            case "removemovie":
+                handleRemoveMovie(event);
+                break;
+
+            case "movielist":
+                handleMovieList(event);
+                break;
+        }
+    }
+
+    private void handleAddMovie(SlashCommandInteractionEvent event){
+        event.reply("Not Implemented yet");
+    }
+
+    private void handleRemoveMovie(SlashCommandInteractionEvent event){
+        event.reply("Not Implemented yet");
+    }
+
+    private void handleMovieList(SlashCommandInteractionEvent event){
+        event.reply("Not Implemented yet");
     }
 
 }
